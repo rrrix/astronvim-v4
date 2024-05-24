d0c2b24a45deac29f30381ef4617171d253792cf
Rick Bowen <rick@monad.com>
2024-05-24 10:24:11 -0700

    M lua/plugins/mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -25,6 +24,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
+        "selene",
         -- add more arguments for adding more null-ls sources
       })
     end,
