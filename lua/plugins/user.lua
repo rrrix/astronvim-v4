86ff6ac674cb29d0b0c5b2ab92bfd347b13a3a5c
Rick Bowen <rbowen@paloaltonetworks.com>
2024-11-15 11:28:27 -0800

    Modified lua/plugins/user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -83,4 +83,37 @@ return {
       vim.keymap.set("n", "<leader>uT", require("tidy").toggle, { desc = "Toggle Strip Trailing Whitespace (Tidy)" })
     end,
   },
+
+    -- Cue support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cue" })
+      end
+    end,
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cueimports" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dagger" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cuelsp", "cueimports" })
+    end,
+  },
 }
